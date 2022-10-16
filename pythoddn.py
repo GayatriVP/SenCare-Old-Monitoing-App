425a19252b620f0b0f6615b02580e6bba8ca1aa7
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2022-10-16 17:46:31 +0530

    Contour detection in progress

--- pythoddn.py
+++ pythoddn.py
@@ -12,19 +12,13 @@ min_confidence = 0.3
 nms_thresh = 0.3
 
 # base path to directory
-base_path = "ly/yolo"
-
-# To count the total number of people
-count = True
-
-# defining minimum distance between two people
-min_distance = 50
+base_path = "yolo"
 
 # Set the threshold value for total violations limit.
 Threshold = 20
 
 # for recorded video, enter path else enter 0 for real time video stream
-url = 0
+url = "videos/fall-01.mp4"
 
 
 # FUNCTION FOR DETECTION OF PEOPLE
@@ -72,11 +66,6 @@ def find(frame, net, ln, person_ID=0):
     # apply non-maxima suppression to suppress weak and overlapping bounding box
     box_id = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, nms_thresh)
 
-    # compute the total people counter
-    if count:
-        human_count = "Human count: {}".format(len(box_id))
-        cv2.putText(frame, human_count, (10, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.60, (0, 0, 0), 2)
-
     # check for at least 1 detection present
     if len(box_id) > 0:
         # loop with indexes
@@ -93,22 +82,16 @@ def find(frame, net, ln, person_ID=0):
     return outputs
 
 
-# GRAB FRAMES FROM VIDEO AND MAKE PREDICTION OF DISTANCE
-
-# construct the argument parse with arguments
-z = argparse.ArgumentParser("Social Distance Detection")
-z.add_argument("-i", "--input", type=str, default="", help="path to (optional) input video file")
-z.add_argument("-o", "--output", type=str, default="", help="path to (optional) output video file")
-z.add_argument("-d", "--display", type=int, default=1, help="whether or not output frame should be displayed")
-args = vars(z.parse_args())
+# output = "videos/output1.mp4"
+display=1
 
 # load COCO class
-labels_path = "ly\yolo\coco.names"
+labels_path = "yolo\coco.names"
 LABELS = open(labels_path).read().strip().split("\n")
 
 # derive paths to YOLO weights and model configuration
-weights_path = "ly\yolo\yolov3.weights"
-config_path = "ly\yolo\yolov3.cfg"
+weights_path = "yolo\yolov3.weights"
+config_path = "yolo\yolov3.cfg"
 
 # load detector trained on COCO dataset
 net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
@@ -117,15 +100,7 @@ net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 ln = net.getLayerNames()
 ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
 
-# if a video path was not supplied, grab a reference to the camera
-if not args.get("input", False):
-    print("[INFO] Starting the video stream...")
-    vs = cv2.VideoCapture(url)
-
-# otherwise, grab a reference to the video file
-else:
-    print("[INFO] Capturing the real-time video...")
-    vs = cv2.VideoCapture(url)
+vs = cv2.VideoCapture(url)
 writer = None
 
 # loop with frames
@@ -145,21 +120,7 @@ while True:
     ruled_out = set()
 
     # check if there are at least 2 people for computing pairwise distance
-    if len(outputs) >= 2:
-        # extract centroids and compute the distance between all pair of centroids
-        centroids = np.array([r[2] for r in outputs])
-        D = dist.cdist(centroids, centroids, metric="euclidean")
-
-        # loop with upper triangular matrix
-        for i in range(0, D.shape[0]):
-            for j in range(i + 1, D.shape[1]):
-
-                # check for safe distance
-                if D[i, j] < min_distance:
-                    # update violation set with index of centroid pairs
-                    ruled_out.add(i)
-                    ruled_out.add(j)
-
+    if len(outputs) >=0:
         # loop with outputs
         for (i, (prob, bbox, centroid)) in enumerate(outputs):
             # extract the bounding box and centroid coordinates, then
@@ -168,36 +129,40 @@ while True:
             (cX, cY) = centroid
             color = (0, 255, 0)
 
-            # if violation occur, update color
-            if i in ruled_out:
-                color = (0, 0, 255)
-
             # draw box & centroid around person detected
             cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
             cv2.circle(frame, (cX, cY), 5, color, 1)
+            im2 = frame[startY:endY,startX:endX]
+            im2 = imutils.resize(im2, width=150, height=150)
+            img_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(img_gray, 90, 255, cv2.THRESH_BINARY)
+            contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-        # display no. of violation count
-        text = "Social distance violation count : {}".format(len(ruled_out))
-        cv2.putText(frame, text, (10, frame.shape[0] - 55), cv2.FONT_HERSHEY_DUPLEX, 0.65, (0, 0, 255), 2)
+            # draw contours on the original image
 
-        threshold = "Threshold limit: {}".format(Threshold)
-        cv2.putText(frame, threshold, (200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.60, (0, 0, 0), 2)
+            image_copy = im2.copy()
+            image_copy = imutils.resize(image_copy, width=150, height=150)
+
+            cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
         # check if output should be displayed or not
-        if args["display"] > 0:
+        if display > 0:
             # show frame
             cv2.imshow("Video Frame", frame)
+            cv2.imshow("Cropped Frame", im2)
+            cv2.imshow('Binary image', thresh)
+            cv2.imshow('None approximation', image_copy)
             key = cv2.waitKey(1) & 0xFF
 
         # "Q" for quit
         if key == ord("q" or "Q"):
             break
 
-    # if o/p path is given and video writer has not initialized
-    if args["output"] != "" and writer is None:
-        # initialize video writer
-        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter(args["output"], fourcc, 25, (frame.shape[1], frame.shape[0]), True)
+    # # if o/p path is given and video writer has not initialized
+    # if output != "" and writer is None:
+    #     # initialize video writer
+    #     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    #     writer = cv2.VideoWriter(output, fourcc, 25, (frame.shape[1], frame.shape[0]), True)
 
     # if the video writer is not None, write the frame to the output video file
     if writer is not None:

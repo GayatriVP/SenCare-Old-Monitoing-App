41e5cf6c09f38a85cb25fc25015d4be56ff74a7e
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2022-10-20 12:54:30 +0530

    ellipse try

--- detect_ellipse.py
+++ detect_ellipse.py
@@ -49,46 +49,27 @@ def plot_boxes(results, frame):
                 if row[4] >= 0.3:
                     x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                     bgr = (0, 255, 0)
-                    # cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                    centerX = int((x1+x2)/2)
+                    centerY = int((y1+y2)/2)
+                    width = int(abs(x1-x2)/2)
+                    height = int(abs(y1-y2)/2)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
                     im2 = frame[y1:y2,x1:x2]
 
                     im2 = imutils.resize(im2, width=150, height=150)
-
-
-                    img_blur = cv2.bilateralFilter(im2, d = 7,sigmaSpace = 75, sigmaColor =75)
-
-                    #gray image
-                    img_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-
-                    #binary thresholding
-                    ret, thresh = cv2.threshold(img_gray, 125, 255, cv2.THRESH_BINARY)
-                    cv2.imshow('gray',thresh)
-
-                    #contouring
-                    items = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-                    contours = items[0] if len(items) == 2 else items[1]
-                    image_copy = im2.copy()
-                    image_copy = imutils.resize(image_copy, width=150, height=150)
-                    
-                    #extreme points
-                    cnts = imutils.grab_contours(items)
-                    c = max(cnts, key=cv2.contourArea)
-                    extLeft = tuple(c[c[:, :, 0].argmin()][0])
-                    extRight = tuple(c[c[:, :, 0].argmax()][0])
-                    extTop = tuple(c[c[:, :, 1].argmin()][0])
-                    extBot = tuple(c[c[:, :, 1].argmax()][0])
-                    cv2.circle(image_copy, extLeft, 8, (0, 0, 255), -1)
-                    cv2.circle(image_copy, extRight, 8, (0, 255, 0), -1)
-                    cv2.circle(image_copy, extTop, 8, (255, 0, 0), -1)
-                    cv2.circle(image_copy, extBot, 8, (255, 255, 0), -1)
-                    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
-                    cv2.imshow('None approximation', image_copy)
-                    # cv2.putText(frame, class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
-
+                    heatmap = cv2.applyColorMap(im2, cv2.COLORMAP_BONE)
+                    ret, thresh = cv2.threshold(heatmap, 205, 255, cv2.THRESH_BINARY)
+                    # items = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+                    # contours = items[0] if len(items) == 2 else items[1]
+                    # cv2.drawContours(image=heatmap, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
+                    # cv2.imshow('thresh', thresh)
+                    cv2.imshow('heatmap', heatmap)
+                    # center = (centerX,centerY)
+                    # imell = cv2.ellipse(frame, center, (width,height),0, 0, 360, (0, 0, 255), 5)
+                    # cv2.imshow("ellipse",imell)
         return frame
 
-url = "videos/fall-04.mp4"
+url = "videos/fall-05.mp4"
 vs = cv2.VideoCapture(url)
 
 while True:
@@ -96,17 +77,16 @@ while True:
     ret, frame = vs.read()
     # assert ret
             
-    frame = imutils.resize(frame, width=1080, height=1080)
-    
+    frame = imutils.resize(frame, width=720, height=720)
 
     # segmentor = SelfiSegmentation()
     # imgOut = segmentor.removeBG(frame, (255,0,255), threshold=0.55)
     # cv2.imshow('new',imgOut)
     
-#     results = score_frame(frame)
-#     frame = plot_boxes(results, frame)
+    results = score_frame(frame)
+    frame = plot_boxes(results, frame)
             
-#     cv2.imshow('YOLOv5 Detection', frame)
+    # cv2.imshow('YOLOv5 Detection', frame)
     if cv2.waitKey(5) & 0xFF == 27:
         break
       

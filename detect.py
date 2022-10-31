7073c265044a5a4984ada93590515af072f9fbfb
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2022-10-31 17:20:31 +0530

    base algo done

--- detect.py
+++ detect.py
@@ -3,102 +3,109 @@ import numpy as np
 import imutils
 import cv2
 from time import time
-import cvzone
-from cvzone.SelfiSegmentationModule import SelfiSegmentation
+# import cvzone
+# from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-classes = model.names
-coord =[]
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+# classes = model.names
+# coord =[]
 # fgbg = cv2.createBackgroundSubtractorMOG2()
 
-def score_frame(frame):
-        """
-        Takes a single frame as input, and scores the frame using yolo5 model.
-        :param frame: input frame in numpy/list/tuple format.
-        :return: Labels and Coordinates of objects detected by model in the frame.
-        """
-        model.to("cpu")
-        frame = [frame]
-        results = model(frame)
-        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+# def score_frame(frame):
+#         """
+#         Takes a single frame as input, and scores the frame using yolo5 model.
+#         :param frame: input frame in numpy/list/tuple format.
+#         :return: Labels and Coordinates of objects detected by model in the frame.
+#         """
+#         model.to("cpu")
+#         frame = [frame]
+#         results = model(frame)
+#         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
         
-        return labels, cord
+#         return labels, cord
 
-def class_to_label(x):
-        """
-        For a given label value, return corresponding string label.
-        :param x: numeric label
-        :return: corresponding string label
-        """
-        return classes[int(x)]
+# def class_to_label(x):
+#         """
+#         For a given label value, return corresponding string label.
+#         :param x: numeric label
+#         :return: corresponding string label
+#         """
+#         return classes[int(x)]
 
-def plot_boxes(results, frame):
-        """
-        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-        :param results: contains labels and coordinates predicted by model on the given frame.
-        :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
-        """
-        labels, cord = results
-        n = len(labels)
-        x_shape, y_shape = frame.shape[1], frame.shape[0]
-        for i in range(n):
-            if labels[i] == 0 or labels[i] == 16:
-                row = cord[i]
-                if row[4] >= 0.3:
-                    x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
-                    bgr = (0, 255, 0)
-                    # cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                    im2 = frame[y1:y2,x1:x2]
+# def plot_boxes(results, frame):
+#         """
+#         Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
+#         :param results: contains labels and coordinates predicted by model on the given frame.
+#         :param frame: Frame which has been scored.
+#         :return: Frame with bounding boxes and labels ploted on it.
+#         """
+#         labels, cord = results
+#         n = len(labels)
+#         x_shape, y_shape = frame.shape[1], frame.shape[0]
+#         for i in range(n):
+#             if labels[i] == 0 or labels[i] == 16:
+#                 row = cord[i]
+#                 if row[4] >= 0.3:
+#                     x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+#                     bgr = (0, 255, 0)
+#                     # cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+#                     im2 = frame[y1:y2,x1:x2]
 
-                    im2 = imutils.resize(im2, width=150, height=150)
-
-
-                    img_blur = cv2.bilateralFilter(im2, d = 7,sigmaSpace = 75, sigmaColor =75)
-
-                    #gray image
-                    img_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+#                     im2 = imutils.resize(im2, width=150, height=150)
 
 
-                    #binary thresholding
-                    ret, thresh = cv2.threshold(img_gray, 125, 255, cv2.THRESH_BINARY)
-                    cv2.imshow('gray',thresh)
+#                     img_blur = cv2.bilateralFilter(im2, d = 7,sigmaSpace = 75, sigmaColor =75)
 
-                    #contouring
-                    items = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-                    contours = items[0] if len(items) == 2 else items[1]
-                    image_copy = im2.copy()
-                    image_copy = imutils.resize(image_copy, width=150, height=150)
+#                     #gray image
+#                     img_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+
+#                     #binary thresholding
+#                     ret, thresh = cv2.threshold(img_gray, 125, 255, cv2.THRESH_BINARY)
+#                     cv2.imshow('gray',thresh)
+
+#                     #contouring
+#                     items = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+#                     contours = items[0] if len(items) == 2 else items[1]
+#                     image_copy = im2.copy()
+#                     image_copy = imutils.resize(image_copy, width=150, height=150)
                     
-                    #extreme points
-                    cnts = imutils.grab_contours(items)
-                    c = max(cnts, key=cv2.contourArea)
-                    extLeft = tuple(c[c[:, :, 0].argmin()][0])
-                    extRight = tuple(c[c[:, :, 0].argmax()][0])
-                    extTop = tuple(c[c[:, :, 1].argmin()][0])
-                    extBot = tuple(c[c[:, :, 1].argmax()][0])
-                    cv2.circle(image_copy, extLeft, 8, (0, 0, 255), -1)
-                    cv2.circle(image_copy, extRight, 8, (0, 255, 0), -1)
-                    cv2.circle(image_copy, extTop, 8, (255, 0, 0), -1)
-                    cv2.circle(image_copy, extBot, 8, (255, 255, 0), -1)
-                    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
-                    cv2.imshow('None approximation', image_copy)
-                    # cv2.putText(frame, class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+#                     #extreme points
+#                     cnts = imutils.grab_contours(items)
+#                     c = max(cnts, key=cv2.contourArea)
+#                     extLeft = tuple(c[c[:, :, 0].argmin()][0])
+#                     extRight = tuple(c[c[:, :, 0].argmax()][0])
+#                     extTop = tuple(c[c[:, :, 1].argmin()][0])
+#                     extBot = tuple(c[c[:, :, 1].argmax()][0])
+#                     cv2.circle(image_copy, extLeft, 8, (0, 0, 255), -1)
+#                     cv2.circle(image_copy, extRight, 8, (0, 255, 0), -1)
+#                     cv2.circle(image_copy, extTop, 8, (255, 0, 0), -1)
+#                     cv2.circle(image_copy, extBot, 8, (255, 255, 0), -1)
+#                     cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
+#                     cv2.imshow('None approximation', image_copy)
+#                     # cv2.putText(frame, class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
 
-        return frame
+#         return frame
+
+
 
 url = "videos/fall-04.mp4"
 vs = cv2.VideoCapture(url)
-
+backSub = cv2.createBackgroundSubtractorKNN()
 while True:
 
     ret, frame = vs.read()
+    if frame is None:
+        break
     # assert ret
             
-    frame = imutils.resize(frame, width=1080, height=1080)
-    
+    # frame = imutils.resize(frame, width=1080, height=1080)
 
+   
+    fgMask = backSub.apply(frame)
+    cv2.imshow('FG Mask', fgMask)
+    cv2.imshow('Frame',frame)
     # segmentor = SelfiSegmentation()
     # imgOut = segmentor.removeBG(frame, (255,0,255), threshold=0.55)
     # cv2.imshow('new',imgOut)
--- detect_ellipse.py
+++ detect_ellipse.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import imutils
+import math
 import cv2
 from time import time
 import cvzone
@@ -10,6 +11,8 @@ from cvzone.SelfiSegmentationModule import SelfiSegmentation
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 classes = model.names
 coord =[]
+backSub = cv2.createBackgroundSubtractorKNN()
+count = 0
 # fgbg = cv2.createBackgroundSubtractorMOG2()
 
 def score_frame(frame):
@@ -40,8 +43,10 @@ def plot_boxes(results, frame):
         :param frame: Frame which has been scored.
         :return: Frame with bounding boxes and labels ploted on it.
         """
+        
         labels, cord = results
         n = len(labels)
+        global count
         x_shape, y_shape = frame.shape[1], frame.shape[0]
         for i in range(n):
             if labels[i] == 0 or labels[i] == 16:
@@ -53,23 +58,61 @@ def plot_boxes(results, frame):
                     centerY = int((y1+y2)/2)
                     width = int(abs(x1-x2)/2)
                     height = int(abs(y1-y2)/2)
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                    ratio = width/height
+                #     center = (centerX,centerY)
+                #     cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
                     im2 = frame[y1:y2,x1:x2]
+                    fgMask = backSub.apply(frame)
+                    fgMask = fgMask[y1:y2,x1:x2]
 
-                    im2 = imutils.resize(im2, width=150, height=150)
-                    heatmap = cv2.applyColorMap(im2, cv2.COLORMAP_BONE)
-                    ret, thresh = cv2.threshold(heatmap, 205, 255, cv2.THRESH_BINARY)
-                    # items = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-                    # contours = items[0] if len(items) == 2 else items[1]
-                    # cv2.drawContours(image=heatmap, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
+                #     im2 = imutils.resize(im2, width=150, height=150)
+                #     heatmap = cv2.applyColorMap(im2, cv2.COLORMAP_BONE)
+                #     ret, thresh = cv2.threshold(heatmap, 205, 255, cv2.THRESH_BINARY)
+                    items = cv2.findContours(image=fgMask, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+                    cnts = imutils.grab_contours(items)
+                #     contours = items[0] if len(items) == 2 else items[1]
+                #     for c in cnts:
+                    M = cv2.moments(fgMask)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    center = (cX,cY)    #b
+                    xaxis = (0,cY)      #a
+                    c = max(cnts, key=cv2.contourArea)
+                    extTop = tuple(c[c[:, :, 1].argmin()][0])   #c
+                #     extBot = tuple(c[c[:, :, 1].argmax()][0])
+                    cv2.circle(im2, extTop, 8, (255, 0, 0), -1)
+                #     cv2.circle(im2, extBot, 8, (255, 255, 0), -1)
+                    cv2.circle(im2, center, 8, (0, 255, 255), -1)
+                    cv2.line(im2,center,extTop,(0,255,0),2)
+                    cv2.line(im2,center,xaxis,(0,255,0),2)
+                    A = math.degrees(math.atan2(extTop[1]-cY, extTop[0]-cX) - math.atan2(cY-cY,0-cX))
+                    if A < 0: A = A + 360
+                    if ratio>1:
+                        if A<75 or A>100:
+                                count+=1
+                    
+                #     print(extTop[1],extTop[0])
+                #     P1 = extTop[0] - 0
+                #     P2 = extTop[1] - cY
+                #     Q1 = cX - 0
+                #     Q2 = cY - cY
+                # #     angle = math.acos((extTop[0]-extTop[1])(cY)/(abs(extTop[1]-extTop[0])*abs(cY-0)))
+
+                #     A = math.acos( (P1*Q1 + P2*Q2) / ( math.sqrt(P1*P1+P2*P2) * math.sqrt(Q1*Q1+Q2*Q2) ) );
+                #     A = A*180/3.14;
+
+                #     print(A)
+                    
+                #     cv2.drawContours(image=im2, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
                     # cv2.imshow('thresh', thresh)
-                    cv2.imshow('heatmap', heatmap)
-                    # center = (centerX,centerY)
+                #     cv2.imshow('fgmask', fgMask)
+                    cv2.imshow('frame', im2)
+                    
                     # imell = cv2.ellipse(frame, center, (width,height),0, 0, 360, (0, 0, 255), 5)
                     # cv2.imshow("ellipse",imell)
         return frame
 
-url = "videos/fall-05.mp4"
+url = "videos/fall-07.mp4"
 vs = cv2.VideoCapture(url)
 
 while True:
@@ -77,7 +120,7 @@ while True:
     ret, frame = vs.read()
     # assert ret
             
-    frame = imutils.resize(frame, width=720, height=720)
+#     frame = imutils.resize(frame, width=720, height=720)
 
     # segmentor = SelfiSegmentation()
     # imgOut = segmentor.removeBG(frame, (255,0,255), threshold=0.55)
@@ -85,6 +128,9 @@ while True:
     
     results = score_frame(frame)
     frame = plot_boxes(results, frame)
+    if count > 5:
+        print("Fall Detected")
+    
             
     # cv2.imshow('YOLOv5 Detection', frame)
     if cv2.waitKey(5) & 0xFF == 27:

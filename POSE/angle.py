060a0cebb6ce56a3cddc16f238f931dbb614030c
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2023-02-23 17:30:17 +0530

    pose angles done

--- POSE/angle.py
+++ POSE/angle.py
@@ -31,27 +31,53 @@ def draw_connections(frame, keypoints, edges, confidence_threshold):
 
 # angles
 def tree_angle(frame, keypoints):
-    left_knee = keypoints[0][0][7]
-    left_ankle = keypoints[0][0][9]
-    x1 = left_knee[0]*192
-    x2 = left_ankle[0]*192
-    y2 = left_ankle[1]*192
-    y1 = left_knee[1]*192
-    # cos = (x2-x1)/(m.sqrt(((x2-x1)**2)+((y2-y1)**2)))
-    tan = (y2-y1)/(x2-x1)
-    a = m.degrees(m.atan(tan))
-    print(a)
-    cv2.line(frame, (int(x1), int(y1)),
-             (int(x2), int(y2)), (0, 0, 255), 2)
-    cv2.imshow('MoveNet Lightning', frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # print("[", keypoints[0][0][5], ",",  keypoints[0][0][6],
+    #       ",", keypoints[0][0][9], ",", keypoints[0][0][10])
+    # print("HEllo")
+    edg = keypoints[0][0].copy()
+    # print(edg)
+    # print(len(edg))
+
+    # temp1 = edg[9]
+    edg[9] = keypoints[0][0][5]
+    edg[5] = keypoints[0][0][9]
+
+    # print("[", keypoints[0][0][5], ",",  keypoints[0][0][6],
+    #       ",", keypoints[0][0][9], ",", keypoints[0][0][10])
+
+    # temp2 = edg[10]
+    edg[10] = keypoints[0][0][6]
+    edg[6] = keypoints[0][0][10]
+
+    # print("[", edg[5], ",", edg[6], ",", edg[9], ",", edg[10])
+
+    angles = []
+    for i in range(5, 13):
+        # First coord
+        a = np.array([edg[i][0], edg[i][1]])
+        # Second coord
+        b = np.array([edg[i+2][0], edg[i+2][1]])
+        # Third coord
+        c = np.array([edg[i+4][0], edg[i+4][1]])
+        radians = np.arctan2(c[1] - b[1], c[0]-b[0]) - \
+            np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+
+        if angle > 180.0:
+            angle = 360-angle
+        angles.append(angle)
+    print(angles)
+    # cv2.line(frame, (int(x1), int(y1)),
+    #          (int(x2), int(y2)), (0, 0, 255), 2)
+    # cv2.imshow('MoveNet Lightning', frame)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 interpreter = tf.lite.Interpreter(
     model_path='POSE/lite-model_movenet_singlepose_lightning_3.tflite')
 interpreter.allocate_tensors()
-img2 = cv2.imread('POSE/DATASET/TRAIN/tree/00000082.jpg')
+img2 = cv2.imread('POSE/DATASET/TRAIN/downdog/00000134.jpg')
 img = tf.image.resize_with_pad(np.expand_dims(img2, axis=0), 192, 192)
 input_image = tf.cast(img, dtype=tf.float32)
 # cv2.imshow('Image', img)
@@ -87,18 +113,18 @@ output_details = interpreter.get_output_details()
 interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
 interpreter.invoke()
 keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
-print(keypoints_with_scores)
-print(img.shape)
+# print(keypoints_with_scores)
+# print(img.shape)
 
 frame = img2.copy()
 frame = cv2.resize(frame, (192, 192))
-tree_angle(frame, keypoints_with_scores)
-# Rendering
-# frame = img2.copy()
-# frame = cv2.resize(frame, (192, 192))
-# draw_connections(frame, keypoints_with_scores, EDGES, 0.0)
-# draw_keypoints(frame, keypoints_with_scores, 0.0)
 
-# cv2.imshow('MoveNet Lightning', frame)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# Rendering
+frame = img2.copy()
+frame = cv2.resize(frame, (192, 192))
+draw_connections(frame, keypoints_with_scores, EDGES, 0.0)
+draw_keypoints(frame, keypoints_with_scores, 0.0)
+tree_angle(frame, keypoints_with_scores)
+cv2.imshow('MoveNet Lightning', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

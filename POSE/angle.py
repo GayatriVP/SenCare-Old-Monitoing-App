5d5297dd1b2661a538f47a01bb70e6a557785fc1
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2023-03-05 18:48:07 +0530

    pose correction done

--- POSE/angle.py
+++ POSE/angle.py
@@ -202,102 +202,116 @@ dict_angles = {1: 'left forearm',
                7: 'left shin',
                8: 'right shin'}
 
-correction = {}
+
+count = 0
 # webcam keypoints
 url1 = "Flask_App/videos/fall-01.mp4"
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
-    img = frame.copy()
-    img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192, 192)
-    input_image = tf.cast(img, dtype=tf.float32)
+    count += 1
+    if count > 10:
+        angle_diff = []
+        img = frame.copy()
+        img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192, 192)
+        input_image = tf.cast(img, dtype=tf.float32)
 
-    # Setup input and output
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+        # Setup input and output
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
 
-    # Make predictions
-    interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
-    interpreter.invoke()
-    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+        # Make predictions
+        interpreter.set_tensor(
+            input_details[0]['index'], np.array(input_image))
+        interpreter.invoke()
+        keypoints_with_scores = interpreter.get_tensor(
+            output_details[0]['index'])
 
-    # Rendering
-    draw_connections(frame, keypoints_with_scores, EDGES, 0.2)
-    draw_keypoints(frame, keypoints_with_scores, 0.2)
-    web_angles = []
-    ke = keypoints_with_scores.copy()
-    kz = ke[0][0]
-    # print(kz)
-    kx = []
-    for a in range(17):
-        # print("in a", kz[a][2])
-        # ke = ke[ke[a][2] < 0.6]
-        if kz[a][2] < 0.4:
-            # print("in if")
-            kx.append(a)
-    knew = np.delete(kz, kx, axis=0)
-    # kz.remove(a)
-    # print(len(knew))
-    if len(knew) == 17:
-        # print("YES")
-        # else:
-        #     print(knew)
-        #         # print("in")
-        web_angles = tree_angle(keypoints_with_scores)
-        print(web_angles)
+        # Rendering
+        draw_connections(frame, keypoints_with_scores, EDGES, 0.2)
+        draw_keypoints(frame, keypoints_with_scores, 0.2)
+        web_angles = []
+        ke = keypoints_with_scores.copy()
+        kz = ke[0][0]
+        # print(kz)
+        kx = []
+        for a in range(17):
+            # print("in a", kz[a][2])
+            # ke = ke[ke[a][2] < 0.6]
+            if kz[a][2] < 0.4:
+                # print("in if")
+                kx.append(a)
+        knew = np.delete(kz, kx, axis=0)
+        # kz.remove(a)
+        # print(len(knew))
+        if len(knew) == 17:
+            # print("YES")
+            # else:
+            #     print(knew)
+            #         # print("in")
+            web_angles = tree_angle(keypoints_with_scores)
+            # print(web_angles)
 
-        filename = 'POSE/pose_angles.csv'
-        pose_angles = []
+            filename = 'POSE/pose_angles.csv'
+            pose_angles = []
 
-        with open(filename, 'r') as file:
-            filez = DictReader(file)
-            for col in filez:
-                pose_angles.append(col['Warrior'])
+            with open(filename, 'r') as file:
+                filez = DictReader(file)
+                for col in filez:
+                    pose_angles.append(col['Warrior'])
 
-        f = 0
-        print(pose_angles)
-        for z in range(8):
-            j = float(pose_angles[z])
-            mn = float(web_angles[z])
-            # print("pose=", type(j),
-            #       " , web=", type(mn))
+            f = 0
+            # print(pose_angles)
+            for z in range(8):
+                j = float(pose_angles[z])
+                mn = float(web_angles[z])
+                # print("pose=", type(j),
+                #       " , web=", type(mn))
 
-            angle_diff = j-mn
-            # print(angle_diff)
-            if abs(angle_diff) >= 15:
-                f = 1
-                if angle_diff < 0:
-                    # print("clock")
-                    correction[z+1] = "clockwise"
-                else:
-                    correction[z+1] = "anticlockwise"
+                angle_diff.append(abs(j-mn))
+            max_angle = max(angle_diff)
+            if max_angle > 25:
+
+                print(angle_diff)
+                ind = angle_diff.index(max_angle)
+                print(ind)
+                part = dict_angles[ind+1]
+                if max_angle < (float(pose_angles[ind])-15):
+                    print("Turn your ", part, "in a clockwise direction.")
+                elif max_angle > (float(pose_angles[ind])+15):
+                    print("Turn your ", part, "in a anticlockwise direction.")
+                    # print(angle_diff)
             else:
-                f = 0
-                # break
-        print(correction)
+                print("Pose done Correctly")
 
+    # # pose-web
+    # # negative toh clockwise
+    # # positive toh anticlockwise
 
-# # pose-web
-# # negative toh clockwise
-# # positive toh anticlockwise
-# # **********************************
+    # # make dictionary of angles
+    # # make dictionary of correction
+    # # output correction
 
-# # make dictionary of angles
-# # make dictionary of correction
-# # process after 5-6 frames
-# # output correction
+    # # first ceck for full body in camera by checking 17 keypoints
+    # # **********************************
 
-# # first ceck for full body in camera by checking 17 keypoints
-# # when len of keypoints is equal to 17 for 5 continuous frames only then calculate angles and start comparing
+    # # process after 10 frames
+    # # for each correction, pick only maximum angle diff and give output
+    # '''correction algo:
+    # make angle diff array
+    # pick max of angle_diff
+    # compare to average range of pose
+    # give correction
+    # '''
+        else:
+            pass
+    #         # print("keypoints not detected")
 
-    else:
-        pass
-#         # print("keypoints not detected")
+        cv2.imshow('MoveNet Lightning', frame)
 
-    cv2.imshow('MoveNet Lightning', frame)
-
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        count = 0
 
 cap.release()
 cv2.destroyAllWindows()
